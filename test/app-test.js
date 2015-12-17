71e4169faeb6bc6308887ba99174dff8e2e0a4b3
AdrieanKhisbe <adriean.khisbe@live.fr>
2015-12-17 13:26:37 +0100

    Test for public routes

--- test/app-test.js
+++ test/app-test.js
@@ -1,10 +1,13 @@
 const Code = require('code');
 const Lab = require('lab');
+const request = require('supertest')
 
 const lab = exports.lab = Lab.script();
 
 const describe = lab.describe;
 const it = lab.it;
+const beforeEach = lab.beforeEach;
+const afterEach = lab.afterEach;
 const expect = Code.expect;
 
 const serverPath = '../server';
@@ -20,10 +23,10 @@ describe('Server', function () {
 
     it('can be started and stop', function (done) {
       var server = require(serverPath)();
-      server.listen(function (err) {
+      var serverListener = server.listen(function (err) {
         expect(err).to.not.exists();
 
-        server.close(done);
+        serverListener.close(done);
 
       })
     })
@@ -31,6 +34,43 @@ describe('Server', function () {
 })
 
 
-describe('Main routes are working', function () {
+describe('Routes', function () {
+  var app, serverListener
+  beforeEach(function (done) {
+    app = require(serverPath)();
+    serverListener = app.listen(done)
+  })
 
+  afterEach(function (done) {
+    serverListener.close(done);
+  })
+
+  describe('static', function () {
+
+    it('serve index implicitely', function (done) {
+      request(app).get('/')
+        .set('Accept','text/html')
+        .expect(200)
+        .expect(function (res) {
+        expect(res.text).to.match(/Seneca Blog/);
+      })
+        .end(done)
+    });
+  });
+
+  it('serve index explicitely', function (done) {
+    request(app).get('/index.html')
+      .expect(200)
+      .expect(function (res) {
+        expect(res.text).to.match(/Seneca Blog/);
+      })
+      .end(done)
+  });
+
+
+  it('404 when needed', function (done) {
+    request(app).get('/404')
+      .expect(404)
+      .end(done)
+  });
 })
